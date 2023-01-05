0a94f227894d502572f0184813bc4d0f78cdd079
Maks2712 <49259635+Maks2712@users.noreply.github.com>
2023-01-05 12:44:52 +0700

    added scopes to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
     has_many :event_lists, foreign_key: :attended_event_id, inverse_of: 'attended_event'
     has_many :attendees, through: :event_lists
     validates :date, presence: true
+    scope :past, ->(datetime) { where("date<?", datetime) }
+    scope :soon, ->(datetime) { where("date>?", datetime) }
 end
